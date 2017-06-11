85de3f8a348e03908fe266b24604d66244cd4a8a
Adam Hartwell <adamhartwell2@gmail.com>
2017-06-11 18:23:35 +0100

    Basic Network Structure

--- data_analysis.py
+++ data_analysis.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[7]:
+# In[3]:
 
 import pandas as pd
 import numpy as np
@@ -13,7 +13,7 @@ with open(file_path) as data_file:
     data = json.load(data_file)
 
 
-# In[21]:
+# In[4]:
 
 i = 0
 for key in data:
@@ -23,12 +23,12 @@ for key in data:
 print("\nSets: {}".format(i))
 
 
-# In[24]:
+# In[5]:
 
 print(data["LEA"]["cards"][0])
 
 
-# In[32]:
+# In[6]:
 
 df_lea = pd.DataFrame(data["LEA"]["cards"])
 
@@ -37,11 +37,26 @@ panel = pd.DataFrame(card_info, index=[0])
 print(panel)
 
 
-# In[38]:
+# In[7]:
 
 df_lea = pd.DataFrame(data["LEA"]["cards"])
 
 card_info = df_lea[['manaCost', 'types', 'toughness', 'power', 'subtypes', 'rarity']]
 panel = pd.DataFrame(card_info)
 print(panel)
+
+
+# In[8]:
+
+df_lea = pd.DataFrame(data["LEA"]["cards"])
+df_emn = pd.DataFrame(data["EMN"]["cards"])
+
+card_info_lea = df_lea[['manaCost', 'types', 'toughness', 'power', 'subtypes', 'text']]
+card_info_emn = df_emn[['manaCost', 'types', 'toughness', 'power', 'subtypes', 'text']]
+panel1 = pd.DataFrame(card_info_lea)
+panel2 = pd.DataFrame(card_info_emn)
+
+frames = [panel1, panel2]
+
+print(pd.concat(frames))
 
--- mtg-net.py
+++ mtg-net.py
@@ -1,16 +1,64 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[8]:
 
 import numpy as np
 import time
 
-from keras.models import Model
+from keras.models import Sequential
 from keras.layers import *
-from keras.optimizers import Adam
-from keras.losses import sparse_categorical_crossentropy
 
 # Data paths
-data_path = 'data'
+data_path = 'data/AllSets.json'
+
+# Training Parameters
+patience = 5  # Early Stopping (using validation macro recall)
+minImprovement = 0.005
+max_epochs = 100
+batch_size = 256
+
+
+# In[14]:
+
+def build_model():
+    '''Method for simply recreating the model when necessary (e.g. between validation folds)
+    '''
+    max_len = 150
+    max_features = 1000
+    
+    model = Sequential()
+    model.add(Embedding(max_features + 1, 128, input_length=max_len))
+    model.add(LSTM(512, activation='tanh', recurrent_activation='hard_sigmoid', use_bias=True, 
+                   kernel_initializer='glorot_uniform', recurrent_initializer='orthogonal', 
+                   bias_initializer='zeros', unit_forget_bias=True, kernel_regularizer=None, 
+                   recurrent_regularizer=None, bias_regularizer=None, activity_regularizer=None, 
+                   kernel_constraint=None, recurrent_constraint=None, bias_constraint=None, 
+                   dropout=0.2, recurrent_dropout=0.2, return_sequences=True))
+    
+    model.add(LSTM(512, activation='tanh', recurrent_activation='hard_sigmoid', use_bias=True, 
+                   kernel_initializer='glorot_uniform', recurrent_initializer='orthogonal', 
+                   bias_initializer='zeros', unit_forget_bias=True, kernel_regularizer=None, 
+                   recurrent_regularizer=None, bias_regularizer=None, activity_regularizer=None, 
+                   kernel_constraint=None, recurrent_constraint=None, bias_constraint=None, 
+                   dropout=0.2, recurrent_dropout=0.2, return_sequences=True))
+    
+    model.add(LSTM(512, activation='tanh', recurrent_activation='hard_sigmoid', use_bias=True, 
+                   kernel_initializer='glorot_uniform', recurrent_initializer='orthogonal', 
+                   bias_initializer='zeros', unit_forget_bias=True, kernel_regularizer=None, 
+                   recurrent_regularizer=None, bias_regularizer=None, activity_regularizer=None, 
+                   kernel_constraint=None, recurrent_constraint=None, bias_constraint=None, 
+                   dropout=0.2, recurrent_dropout=0.2, return_sequences=False))
+    model.add(Dense(max_len, activation='softmax'))
+
+    
+    model.compile(optimizer='rmsprop',
+                  loss='binary_crossentropy',
+                  sample_weight_mode='None',
+                  metrics=['accuracy'])
+    
+    return model
+
+model = build_model()    
+model.summary()
 
